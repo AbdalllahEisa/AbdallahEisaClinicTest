829f3d19084d96500fb15fea85e518f994a0c061
AbdalllahEisa <89872569+AbdalllahEisa@users.noreply.github.com>
2021-09-01 08:35:12 +0200

    Delete AdminPanel.java

--- src/main/java/com/example/test/dao/AdminPanel.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.dao;
-
-import org.springframework.data.jpa.mapping.JpaPersistentEntity;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface AdminPanel extends JpaRepository<Clinic,Long> {
-
-
-}
