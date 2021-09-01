6b33bd047af512191d25bfbd9cf4336ac59b3afd
AbdalllahEisa <89872569+AbdalllahEisa@users.noreply.github.com>
2021-09-01 08:34:53 +0200

    Update AdminPanel.java

--- src/main/java/com/example/test/dao/AdminPanel.java
+++ src/main/java/com/example/test/dao/AdminPanel.java
@@ -3,7 +3,7 @@ package com.example.test.dao;
 import org.springframework.data.jpa.mapping.JpaPersistentEntity;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
-public interface AdminPanel extends JpaRepositoryImplementation<Clinic,Long> {
+public interface AdminPanel extends JpaRepository<Clinic,Long> {
 
 
 }
